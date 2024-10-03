7e5fb51e0758cf31dc5639f70d57ce584cbd4f2b
Usuario <Usuario@DESKTOP-13P5TSU>
2024-10-03 19:53:16 +0200

    Master branch commit

--- src/testPackage/TestClass.java
+++ src/testPackage/TestClass.java
@@ -4,7 +4,7 @@ public class TestClass {
 	
 	public void testMethod() {
 		System.out.println("Testing...");
-		System.out.println("Second commit");
+		System.out.println("Master branch commit");
 		System.out.println("Third change");
 		System.out.println("Fourth change");
 		System.out.println("Fifth change");
